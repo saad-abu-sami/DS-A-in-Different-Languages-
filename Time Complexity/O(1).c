326ac2580f08c0aa8dc76a394787b42ceb67eca9
Saad Abu Sami <saadabusami061@gmail.com>
2022-02-11 13:02:43 +0600

    Data Structure and Algorithm In All Language

---
+++ Time Complexity/O(1).c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+int main(){
+   printf("Hello world");
+   /* In above code “Hello World!!!” print only once on a screen.So, time complexity is constant:
+   O(1) i.e. every time constant amount of time require to execute code,
+   no matter which operating system or which machine configurations you are using.
+   */
+}
